5a583f6dc705dc1ac62fec853a30e9c53ba12f0c
Duc <nhduc.job@gmail.com>
2026-03-26 01:00:21 +0700

    [UC-01] Tạo thuộc tính cơ bản cho Vehicle model
    [UC-01] Hoàn thiện thuộc tính cho Vehicle

---
+++ Models/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalVehicleService.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int VehicleId { get; set; }
+
+        [Required(ErrorMessage = "Tên dòng xe không được để trống")]
+        public string VehicleModel { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Pin phải từ 0% đến 100%")]
+        public int BatteryPercentage {  get; set; }
+
+        public string State { get; set; } // Ví dụ: "Sẵn sàng", "Đang thuê", "Hỏng", "Đang bảo trì"
+
+        public int? CurrentStationId { get; set; }
+
+        public bool IsPluggedIn { get; set; }
+
+        //Chỉ dùng để kiểm tra, không lưu Database
+        public bool IsReadyForRent => BatteryPercentage > 15 && !IsPluggedIn;
+    }
+}
